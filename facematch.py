cd56e309abadf084538c4035477f13e3a861b7ed
TheCodingStache <cs4415161@teilar.gr>
2019-12-13 16:17:43 +0200

    Deployment of face match and face recognition also can find how many faces appear on a picture

--- facematch.py
+++ facematch.py
@@ -1,15 +1,14 @@
 import face_recognition
 
 image_of_brother = face_recognition.load_image_file('img/known/Vassilis_Pallas.jpg')
-vassilis_face_encoding = face_recognition.face_locations(image_of_brother)[0]
+vassilis_face_encoding = face_recognition.face_encodings(image_of_brother)[0]
 
-unknown_image = face_recognition.load_image_file('img/unknown/we_will_see.jpg')
-unknown_face_encoding = face_recognition.face_locations(unknown_image)[0]
+unknown_image = face_recognition.load_image_file('img/unknown/haha.jpg')
+unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
 
 # Compare faces
-
 results = face_recognition.compare_faces(
-    [vassilis_face_encoding], [unknown_face_encoding])
+    [vassilis_face_encoding], unknown_face_encoding)
 
 if results[0]:
     print('This is vassilis')
