cd99d968d08e425fd3ddd59b3bcc846b99b31b2c
TheCodingStache <cs4415161@teilar.gr>
2019-12-17 16:43:42 +0200

    Now ML identifies the familiar faces from a group picture

--- facematch.py
+++ facematch.py
@@ -3,7 +3,7 @@ import face_recognition
 image_of_me = face_recognition.load_image_file('img/known/Dimitris_Pallas.jpg')
 dimitris_face_encoding = face_recognition.face_encodings(image_of_me)[0]
 
-unknown_image = face_recognition.load_image_file('img/unknown/Lazos_Karathanasis.jpg')
+unknown_image = face_recognition.load_image_file('img/unknown/we_will_see.jpg')
 unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
 
 # Compare faces
@@ -11,6 +11,6 @@ results = face_recognition.compare_faces(
     [dimitris_face_encoding], unknown_face_encoding)
 
 if results[0]:
-    print('This is Dimitris')
+    print('Dimitris is in the picture')
 else:
-    print('This is NOT Dimitris')
+    print('Dimitris is not in the picture')
